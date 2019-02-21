f6f9316671a768dea671ad68a8727f3c425008b7
Kingson Chan <kingson@simprints.com>
2019-02-21 01:50:20 +0000

    Refactor green robot tracking

--- experimental/green_robot_tracking.py
+++ experimental/green_robot_tracking.py
@@ -10,8 +10,10 @@ import math
 import numpy as np
 import cv2
 
+# Better list with max length
 tracked_pts = deque(maxlen=100)
-USE_LIVE_CAM = False
+
+USE_LIVE_CAM = True
 
 if USE_LIVE_CAM:
     cap = cv2.VideoCapture(cv2.CAP_DSHOW + 1)
@@ -20,6 +22,17 @@ if USE_LIVE_CAM:
 else:
     # Open sample video
     cap = cv2.VideoCapture('test_files/table3_sample.wmv')
+
+cv2.namedWindow('Tracking', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('Tracking', 1200,600)
+
+# Create output video
+# fourcc = cv2.VideoWriter_fourcc(*'XVID')
+# out = cv2.VideoWriter('output3.avi',fourcc, 20.0, (1600,1200))
+
+# GREEN TAPE
+lower_thresh = np.array([60, 100, 40])
+upper_thresh = np.array([95, 255, 255])
 
 def standard_transform(frame):
     # Coordinates for perspective transform
@@ -42,19 +55,8 @@ def standard_transform(frame):
 
     return frame
 
-cv2.namedWindow('Tracking', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('Tracking', 1200,600)
-
 # Create tracker
 tracker = cv2.TrackerMOSSE_create()
-
-# Create output video
-# fourcc = cv2.VideoWriter_fourcc(*'XVID')
-# out = cv2.VideoWriter('output3.avi',fourcc, 20.0, (1600,1200))
-
-# GREEN TAPE
-lower_thresh = np.array([60, 100, 40])
-upper_thresh = np.array([95, 255, 255])
 
 # Read first frame
 ret, frame = cap.read()
@@ -65,17 +67,17 @@ mask = cv2.inRange(hsv, lower_thresh, upper_thresh)
 masked = cv2.bitwise_and(frame, frame, mask=mask)
 gray = cv2.cvtColor(masked, cv2.COLOR_BGR2GRAY)
 
-# Find circle
+# Find circle in gray masked frame
 circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 20, minRadius=20, maxRadius=60)
 print(circles)
 
 x_center, y_center, radius = circles[0]
 
-# (xmin, ymin, width, height)
+# Region of interest bounding box (xmin, ymin, width, height)
 bbox = (x_center-(radius+20), y_center-(radius+20), 2*(radius+20), 2*(radius+20))
 
-# Initialise tracker
-ret = tracker.init(frame, bbox)
+# Initialise tracker in masked frame
+ret = tracker.init(masked, bbox)
 
 while True:
     ret, frame = cap.read()
@@ -85,9 +87,11 @@ while True:
 
     # Convert to HSV colourspace for colour thresholding
     hsv = cv2.cvtColor(dst, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower_thresh, upper_thresh)
+    masked = cv2.bitwise_and(frame, frame, mask=mask)
 
-    # Update tracker
-    ret, bbox = tracker.update(frame)
+    # Update tracker in masked frame
+    ret, bbox = tracker.update(masked)
 
     if ret:
         # Object found
@@ -95,20 +99,21 @@ while True:
         p2 = (int(bbox[0] + bbox[2]), int(bbox[1]+  bbox[3]))
         cv2.rectangle(frame, p1, p2, (0,0,255), 2, 1)
         tracked_center = (bbox[0]+bbox[2]/2, bbox[1]+bbox[3]/2)
-        tracked_pts.append(tracked_center)
+        tracked_pts.appendleft(tracked_center)
     else :
         # Cannot find object
         cv2.putText(frame, "Track failed", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
 
+    # Draw path of robot
     for i in range(len(tracked_pts)):
         if i == 0:
             continue
-
         cv2.line(frame, tracked_pts[i-1], tracked_pts[i], (0,0,255), 2)
 
+    # Calculate orientation based on previous position (from 10 frames back)
     if len(tracked_pts) >= 10:
-        dx = pts[-1][0] - pts[-10][0]
-        dy = pts[-1][1] - pts[-10][1]
+        dx = pts[0][0] - pts[10][0]
+        dy = pts[0][1] - pts[10][1]
 
         magnitude = math.sqrt(dx**2 + dy**2)
         dx = dx/magnitude
@@ -117,6 +122,7 @@ while True:
         cv2.arrowedLine(tracked_center, (tracked_center[0]+dx, tracked_center[1]+dy), \
                         (255,0,0), 4)
 
+    # Calculate frame rate
     fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
     cv2.putText(frame, "FPS: " + str(int(fps)), (100,50), \
                 cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
