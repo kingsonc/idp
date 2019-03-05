145879ffdcfa8fcc3ef830b3c983e39f5767cc2d
lyndonteng <lyndon@live.com.sg>
2019-03-05 17:55:24 +0000

    Update infrared_beam_Break.ino
    calibration script

--- arduino/infrared/infrared_beam_Break/infrared_beam_Break.ino
+++ arduino/infrared/infrared_beam_Break/infrared_beam_Break.ino
@@ -7,10 +7,4 @@
  void loop() {   
   int val = analogRead(A0);
   Serial.println(val);          // prints the values from the sensor in serial monitor  
-  if (val >= 700) {
-    //run subroutine here
-    Serial.println("There is a block in the way!");
-  }
-  
-  delay(100);
   }  
