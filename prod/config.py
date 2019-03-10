745b920c78c2d31c2b27b17ca025d9dd4f1d1691
Kingson Chan <kingson@simprints.com>
2019-03-10 19:15:47 +0000

    Relax robot tracking colour threshold

--- prod/config.py
+++ prod/config.py
@@ -16,8 +16,8 @@ class Config:
     BLUE_UPPER_THRESH = np.array([120, 255, 255])
 
     # Robot tracking
-    GREEN_LOWER_THRESH = np.array([60, 100, 120])
-    GREEN_UPPER_THRESH = np.array([90, 255, 255])
+    GREEN_LOWER_THRESH = np.array([60, 100, 90])
+    GREEN_UPPER_THRESH = np.array([95, 255, 255])
 
     MAP_COORDS = np.float32([[1425,1395],[1473,604],[0,482],[0,977]])
 
@@ -50,23 +50,3 @@ class Table3Config(Config):
     # Padding to cover missing area not visible in camera
     TABLE_BORDER_FILL_LEFT = 74
     TABLE_BORDER_FILL_RIGHT = 199
-
-
-class Table2Config(Config):
-    """Specific values only applicable to table 3"""
-    CAM_BRIGHTNESS = 60
-    CAM_CONTRAST = 48
-    CAM_SATURATION = 70
-
-    # Perspective transformation
-    CAMERA_COORDS = np.float32([[1417,1393],[1466,606],[10,495],[4,999]])
-
-    # Trim to only display table area
-    TABLE_EDGE_TOP = 55
-    TABLE_EDGE_BOT = 1528
-
-    # Padding to cover missing area not visible in camera
-    TABLE_BORDER_FILL_LEFT = 74
-    TABLE_BORDER_FILL_RIGHT = 199
-
-current_config = Table3Config()
